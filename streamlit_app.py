c216eb09720eef67cdfdaea4b09ae31a140cd5fa
yas048 <giastegar@gmail.com>
2025-05-27 09:35:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,13 @@ st.title("Selamat Datang di Web Informatika")
 st.write(
     "ngoding seru bersama Ghiyas Tegar"
 )
-st.image("bebek.jpg")
+st.image("bebek.jpg", width=200)
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1
+
+if (angka % 2) -- 0
+  st.write(f"(angka) adalah Bilangan Genap")
+else:
+st.write(f"(angka) adalah Bilangan Ganjil
